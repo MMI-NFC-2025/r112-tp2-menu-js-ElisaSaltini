c6ee52afbb8d0adf422e86a1e40b04707d5f2fdb
ElisaSaltini <elisa.saltini@edu.univ-fcomte.fr>
2025-11-12 11:54:10 +0100

    Ouverture menu Elisa Saltini

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// On cible les éléments à modifier
+const nav = document.querySelector("nav");
+const toggle = document.querySelector(".menu-btn");
+
+// Ajoute un écouteur d'événements au bouton 'toggle'
+toggle.addEventListener("click", () => {
+    const isOpen = toggle.getAttribute("aria-expanded") === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen :", isOpen, "isClosed :", isClosed);
+
+    // Mise à jour des attributs ARIA pour l'accessibilité
+    nav.ariaHidden = "false";
+    toggle.ariaExpanded = "true";
+});
